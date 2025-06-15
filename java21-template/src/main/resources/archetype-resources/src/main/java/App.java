b40318e378db02c722cf2e46c303971990536fe0
Matthew Gerstemeier <mgerstemeier@cas.org>
2025-06-14 20:48:13 -0400

    adding in slf4j logging

--- java21-template/src/main/resources/archetype-resources/src/main/java/App.java
+++ java21-template/src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,8 +1,11 @@
 package ${package};
 
-public class App {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
+public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+    public static void main(String[] args) {
+        logger.info("Hello World");
+    }
 }
