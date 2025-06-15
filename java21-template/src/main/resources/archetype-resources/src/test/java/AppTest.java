58ec363054dd2e50b92a60d5802b37750d61f6be
Matthew Gerstemeier <mgerstemeier@cas.org>
2025-06-14 20:11:20 -0400

    removing older project versions and updating maven assembly jar plugin

---
+++ java21-template/src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,14 @@
+package ${package};
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+  @Test
+  @DisplayName("Hello World")
+  void test() {
+        assert(true);
+  }
+}
